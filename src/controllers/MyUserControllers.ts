d5f727192710970fc7de74405135e16a2d99836c
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-05-11 23:06:06 +0530

    validation added in user profile updation and also added api route for update user profile and also fetch data from token after decoding the token of auth0

--- src/controllers/MyUserControllers.ts
+++ src/controllers/MyUserControllers.ts
@@ -28,9 +28,38 @@ const createCurrentUser = async (req: Request, res: Response) => {
     console.log(error.message);
     res.status(500).json({
       message: "Error creating creatCurrentUser",
-      error: error?.message,
     });
   }
 };
 
-export default { createCurrentUser };
+export const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.country = country;
+    user.city = city;
+
+    await user.save();
+
+    res.status(200).json({
+      message: "User updated successfully",
+      user: user.toObject(),
+    });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Error while updating current user",
+    });
+  }
+};
+
+export default { createCurrentUser, updateCurrentUser };
--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import MyUserControllers from "../controllers/MyUserControllers";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
-router.post("/",jwtCheck, MyUserControllers.createCurrentUser)
+router.post("/", jwtCheck, MyUserControllers.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserControllers.updateCurrentUser
+);
+
+
 
 export default router;
