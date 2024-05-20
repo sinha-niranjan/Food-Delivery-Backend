82b7ac3568b11c83244eb235776c36a353d2a2ef
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-05-21 05:20:25 +0530

    create rest

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ mongoose
   .then(() => console.log("Connected to databse"));
 
 cloudinary.config({
-  clound_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
